1644aa2421bb42e6f75f66e909e41e939917b3c6
AbhinandanBM <bmabhi13@gmail.com>
2024-01-23 17:55:18 +0530

    Insertion

---
+++ 12.Linked List/Singly Linked List/Insertion/05_create_list.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int value)
+    {
+        data = value;
+        next = NULL;
+    }
+};
+
+Node *CreateLinkedList(int arr[], int index, int size)
+{
+    if (index == size)
+    {
+        return NULL;
+    }
+
+    Node *temp = new Node(arr[index]);
+    temp->next = CreateLinkedList(arr, index + 1, size);
+    return temp;
+}
+
+int main()
+{
+    Node *Head = NULL;
+
+    int arr[] = {2, 4, 5, 6, 8};
+
+    Head = CreateLinkedList(arr, 0, 5);
+
+    Node *temp = Head;
+    while (temp)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    return 0;
+}
