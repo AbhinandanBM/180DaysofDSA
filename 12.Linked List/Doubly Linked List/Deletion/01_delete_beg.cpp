9e6aec7b5be8bb8b2555f677e4892de6364ad94f
AbhinandanBM <bmabhi13@gmail.com>
2024-01-29 17:49:59 +0530

    Deletion in DLL

---
+++ 12.Linked List/Doubly Linked List/Deletion/01_delete_beg.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node *prev;
+
+    Node(int value)
+    {
+        data = value;
+        next = NULL;
+        prev = NULL;
+    }
+};
+
+Node *createList(int value, Node *head)
+{
+    if (head == NULL)
+    {
+        head = new Node(value);
+    }
+    else
+    {
+        Node *curr = head;
+        while (curr->next != NULL)
+        {
+            curr = curr->next;
+        }
+        Node *temp = new Node(value);
+        curr->next = temp;
+        temp->prev = curr;
+    }
+    return head;
+}
+
+void Traverse(Node *head)
+{
+    Node *temp = head;
+    cout << "\nThe Nodes are:" << endl;
+    while (temp)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+Node *delete_beg(Node *head)
+{
+    if (head == NULL)
+    {
+        cout << "Deletion is not Possible" << endl;
+    }
+    else if (head->next == NULL)
+    {
+        delete head;
+        head = NULL;
+    }
+    else
+    {
+        Node *temp = head;
+        head = head->next;
+        delete temp;
+        head->prev = NULL;
+    }
+    return head;
+}
+
+int main()
+{
+    int n, value;
+    cout << "Enter the size:";
+    cin >> n;
+    Node *head = NULL;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter the value:";
+        cin >> value;
+        head = createList(value, head);
+    }
+
+    Traverse(head);
+
+    head = delete_beg(head);
+
+    cout << "\nAfter deletion at begining:";
+    Traverse(head);
+    return 0;
+}
